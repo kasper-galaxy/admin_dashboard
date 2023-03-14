77430552bac8463272631ca629c1cecff4749d98
kasper-galaxy <eugene.supersenior@outlook.com>
2023-03-14 03:53:33 -0400

    star Icon

--- src/pages/icons.js
+++ src/pages/icons.js
@@ -1,9 +1,9 @@
-import { Helmet } from 'react-helmet-async';
-import CogIcon from '@heroicons/react/24/solid/CogIcon';
-import DocumentTextIcon from '@heroicons/react/24/solid/DocumentTextIcon';
-import HomeIcon from '@heroicons/react/24/solid/HomeIcon';
-import ShoppingCartIcon from '@heroicons/react/24/solid/ShoppingCartIcon';
-import StarIcon from '@heroicons/react/24/solid/StarIcon';
+import { Helmet } from "react-helmet-async";
+import CogIcon from "@heroicons/react/24/solid/CogIcon";
+import DocumentTextIcon from "@heroicons/react/24/solid/DocumentTextIcon";
+import HomeIcon from "@heroicons/react/24/solid/HomeIcon";
+import ShoppingCartIcon from "@heroicons/react/24/solid/ShoppingCartIcon";
+import StarIcon from "@heroicons/react/24/solid/StarIcon";
 import {
   Box,
   Card,
@@ -12,56 +12,40 @@ import {
   Stack,
   SvgIcon,
   Typography,
-  Unstable_Grid2 as Grid
-} from '@mui/material';
+  Unstable_Grid2 as Grid,
+} from "@mui/material";
 
 const icons = [
   <CogIcon />,
   <DocumentTextIcon />,
   <HomeIcon />,
   <ShoppingCartIcon />,
-  <StarIcon />
+  // <StarIcon />
 ];
 
 const Page = () => (
   <>
     <Helmet>
-      <title>
-        Icons | Carpatin Free
-      </title>
+      <title>Icons | Carpatin Free</title>
     </Helmet>
     <Box
       sx={{
         flexGrow: 1,
-        py: 8
+        py: 8,
       }}
     >
       <Container maxWidth="xl">
         <Stack spacing={3}>
           <div>
-            <Typography variant="h4">
-              Icons
-            </Typography>
+            <Typography variant="h4">Icons</Typography>
           </div>
           <div>
-            <Grid
-              container
-              spacing={3}
-            >
-              <Grid
-                xs={12}
-                md={4}
-              >
+            <Grid container spacing={3}>
+              <Grid xs={12} md={4}>
                 <Stack spacing={1}>
-                  <Typography variant="h6">
-                    Hero Icons
-                  </Typography>
-                  <Typography
-                    color="text.secondary"
-                    variant="body2"
-                  >
-                    We use
-                    {' '}
+                  <Typography variant="h6">Hero Icons</Typography>
+                  <Typography color="text.secondary" variant="body2">
+                    We use{" "}
                     <Link
                       color="primary"
                       href="https://heroicons.com"
@@ -69,29 +53,17 @@ const Page = () => (
                       variant="inherit"
                     >
                       Hero Icons
-                    </Link>
-                    {' '}
-                    for displaying icons as we think it reflects the clean
-                    and light style of the Carpatin Design System.
+                    </Link>{" "}
+                    for displaying icons as we think it reflects the clean and light style of the
+                    Carpatin Design System.
                   </Typography>
                 </Stack>
               </Grid>
-              <Grid
-                xs={12}
-                md={8}
-              >
+              <Grid xs={12} md={8}>
                 <Card>
-                  <Stack
-                    alignItems="center"
-                    direction="row"
-                    spacing={2}
-                    sx={{ p: 3 }}
-                  >
+                  <Stack alignItems="center" direction="row" spacing={2} sx={{ p: 3 }}>
                     {icons.map((icon, index) => (
-                      <SvgIcon
-                        key={index}
-                        sx={{ color: 'neutral.600' }}
-                      >
+                      <SvgIcon key={index} sx={{ color: "neutral.600" }}>
                         {icon}
                       </SvgIcon>
                     ))}
