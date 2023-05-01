058451e495244258a318002aaad4263f1d705e94
kasper-galaxy <eugene.supersenior@outlook.com>
2023-05-01 13:57:18 -0400

    last commit

--- src/pages/icons.js
+++ src/pages/icons.js
@@ -46,7 +46,7 @@ const Page = () => (
                       target="_blank"
                       variant="inherit"
                     >
-                      {/* Hero Icons */}
+                      Hero Icons111
                     </Link>{" "}
                     for displaying icons as we think it reflects the clean and light style of the
                     Carpatin Design System. ///
